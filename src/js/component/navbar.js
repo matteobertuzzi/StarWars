d95030f837fc031caf1ebbefbb1b9075c716f770
Matteo Bertuzzi <146861948+matteobertuzzi@users.noreply.github.com>
2024-01-31 20:27:57 +0000

    update navbar

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -3,9 +3,18 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
+		<nav className="navbar navbar-dark bg-dark m-4">
 			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				<span className="navbar-brand mb-0 h1">Home</span>
+			</Link>
+			<Link to="/characters">
+				<span className="navbar-brand mb-0 h1">Charachters</span>
+			</Link>
+			<Link to="/planets">
+				<span className="navbar-brand mb-0 h1">Planets</span>
+			</Link>
+			<Link to="/vehicles">
+				<span className="navbar-brand mb-0 h1">Vehicles</span>
 			</Link>
 		</nav>
 	);
